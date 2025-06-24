2114cdb1d89af72613f9694f8cf439dcab1509bb
Tamás Borbély <menzaboyy@gmail.com>
2025-06-24 18:04:50 +0200

    remove dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/TI-Stats/',
-  plugins: [react()],
+export default defineConfig(({command }) => {
+  return {
+    base: command === 'serve' ? '/' : "/TI-Stats/",
+    plugins: [react()],
+  }
 });
